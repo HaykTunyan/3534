88026de4b52ceec4d37708d79e263ef38d0b92ee
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-08-27 18:20:59 +0400

    added datatable

--- app.js
+++ app.js
@@ -109,65 +109,105 @@ app.get('/item', function (req, res) {
     }
 
 
+
+
     let avg_of_starts = 0, avg_of_transfers = 0, avg_of_arc_hours = 0;
     axios.get('https://lib-mtconnect-cartridge.azurewebsites.net/assets').then(result => {
 
         let data = JSON.parse(xml_js.xml2json(result.data, { compact: true })).
             MTConnectAssets.Assets.HyperthermCartridge;
 
-        let TempDataArray = [];
-
+        let temp = [];
+        let DataArray = [];
         for (let i = 0; i < data.length; i++) {
-            avg_of_starts += parseInt(data[i].OperationalData.NumberOfStarts._text);
-            avg_of_transfers += parseInt(data[i].OperationalData.NumberOfTransfers._text);
-            avg_of_arc_hours += parseInt(data[i].OperationalData.ArcTime._text);
+            // avg_of_starts += parseInt(data[i].OperationalData.NumberOfStarts._text);
+            // avg_of_transfers += parseInt(data[i].OperationalData.NumberOfTransfers._text);
+            // avg_of_arc_hours += parseInt(data[i].OperationalData.ArcTime._text);
 
-            let new_data = {
-                manufacturer: data[i]._attributes.manufacturer,
-                serialNumber: data[i]._attributes.serialNumber,
-                timestamp: new Date(data[i]._attributes.timestamp),
-                deviceUuid: data[i]._attributes.deviceUuid,
-                assetId: data[i]._attributes.assetId,
-                ManufacturingData_Description: data[i].ManufacturingData.Description._text,
-                UUID: data[i].ManufacturingData.UUID._text,
-                PartNumber: data[i].ManufacturingData.PartNumber._text,
-                PartNumberRevision: data[i].ManufacturingData.PartNumberRevision._text,
-                CartridgeType: data[i].ManufacturingData.CartridgeType._text,
-                CartridgeDesignRevision: data[i].ManufacturingData.CartridgeDesignRevision._text,
-                ManufacturingDate: data[i].ManufacturingData.ManufacturingDate._text,
-                ManufacturingTestStatus: data[i].ManufacturingData.ManufacturingTestStatus._text,
-                OperationalData_Description: data[i].OperationalData.Description._text,
-                ArcTime: parseInt(data[i].OperationalData.ArcTime._text),
-                PilotTime: parseInt(data[i].OperationalData.PilotTime._text),
-                TransferTime: parseInt(data[i].OperationalData.TransferTime._text),
-                NumberOfStarts: parseInt(data[i].OperationalData.NumberOfStarts._text),
-                NumberOfTransfers: parseInt(data[i].OperationalData.NumberOfTransfers._text),
-                Faults: data[i].OperationalData.Faults._attributes.faultCount > 0 ? FaulttoStringArray(data[i].OperationalData.Faults.Fault) : '',
-                EndOfLifeEventCount: parseInt(data[i].OperationalData.EndOfLifeEventCount._text)
-            }
-            TempDataArray.push(new_data);
+            // Manufacturer	
+            // Asset ID	
+            // UUID	
+            // Number Of Starts	
+            // Number Of Transfers	
+            // Manufacturing Date	
+            // Part Number	
+            // Timestamp
+
+            DataArray.push([
+                data[i]._attributes.manufacturer,
+                data[i]._attributes.assetId,
+                data[i].ManufacturingData.UUID._text,
+                parseInt(data[i].OperationalData.NumberOfStarts._text),
+                parseInt(data[i].OperationalData.NumberOfTransfers._text),
+                data[i].ManufacturingData.ManufacturingDate._text,
+                data[i].ManufacturingData.PartNumber._text,
+                new Date(data[i]._attributes.timestamp)
+            ])
+
+            // DataArray.push([
+
+            //     data[i]._attributes.manufacturer,
+            //     data[i]._attributes.serialNumber,
+            //     new Date(data[i]._attributes.timestamp),
+            //     data[i]._attributes.deviceUuid,
+            //     data[i]._attributes.assetId,
+            //     data[i].ManufacturingData.Description._text,
+            //     data[i].ManufacturingData.UUID._text,
+            //     data[i].ManufacturingData.PartNumber._text,
+            //     data[i].ManufacturingData.PartNumberRevision._text,
+            //     data[i].ManufacturingData.CartridgeType._text,
+            //     data[i].ManufacturingData.CartridgeDesignRevision._text,
+            //     data[i].ManufacturingData.ManufacturingDate._text,
+            //     data[i].ManufacturingData.ManufacturingTestStatus._text,
+            //     data[i].OperationalData.Description._text,
+            //     parseInt(data[i].OperationalData.ArcTime._text),
+            //     parseInt(data[i].OperationalData.PilotTime._text),
+            //     parseInt(data[i].OperationalData.TransferTime._text),
+            //     parseInt(data[i].OperationalData.NumberOfStarts._text),
+            //     parseInt(data[i].OperationalData.NumberOfTransfers._text),
+            //     data[i].OperationalData.Faults._attributes.faultCount > 0 ? FaulttoStringArray(data[i].OperationalData.Faults.Fault) : '',
+            //     parseInt(data[i].OperationalData.EndOfLifeEventCount._text)
+            // ])
+            //  DataArray.push(temp);
+            //temp.splice(0);
+            // temp.splice(0);
         }
 
-
-
-
-        if (_.isEmpty(req.query)) {
-            res.json({
-                data_array: TempDataArray,
-                avg_of_starts: Math.round(avg_of_starts / data.length),
-                avg_of_transfers: Math.round(avg_of_transfers / data.length),
-                avg_of_st: Math.round((avg_of_starts + avg_of_transfers) / data.length),
-                avg_of_arc_hours: Math.round(avg_of_arc_hours / data.length)
-            });
-        } else {
-            res.json({
-                data_array: TempDataArray.filter((elem) => {
-                    return elem[filterBy] == value
-                }),
-            });
-        }
-
+        console.log(DataArray);
+        res.json({
+            "data": DataArray
+        });
     })
+
+
+    // )
+    // TempDataArray.push(new_data);
+    //DataArray.push(temp);
+    //temp.splice(0);
+    // console.log(data);
+    // }
+
+
+    //res.send(TempDataArray);
+
+
+    // if (_.isEmpty(req.query)) {
+    //     res.json({
+    //         data_array: TempDataArray,
+    //         avg_of_starts: Math.round(avg_of_starts / data.length),
+    //         avg_of_transfers: Math.round(avg_of_transfers / data.length),
+    //         avg_of_st: Math.round((avg_of_starts + avg_of_transfers) / data.length),
+    //         avg_of_arc_hours: Math.round(avg_of_arc_hours / data.length)
+    //     });
+    // } else {
+    //     res.json({
+    //         data_array: TempDataArray.filter((elem) => {
+    //             return elem[filterBy] == value
+    //         }),
+    //     });
+    // }
+
+
 })
 
 //  http://localhost:8000/item
